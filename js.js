330e1b509e82445d6c3c07db6fa021985a84122c
dsdsgege <gellerr.vrabely@gmail.com>
2024-08-27 15:51:06 +0200

    slide done

--- js.js
+++ js.js
@@ -3,24 +3,42 @@ const slides = document.querySelectorAll('.slide');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 const totalSlides = slides.length;
+let index = 0;
+
+
+prev.addEventListener("click",function() {
+    index--;
+    if(index < 0) {
+        index = totalSlides-1;
+    }
+    showItems();
+
+}
+
+)
+
+next.addEventListener("click",function() {
+    index++;
+    if(index >= totalSlides) {
+        index = 0;
+    }
+    showItems();
+
+}
+
+)
 
 function showItems() {
     for(let i = 0; i < slides.length; i++) {
         slides[i].classList.add("hide");
+        slides[i].classList.remove("show");
     }
+    slides[index].classList.add("show");
+    slides[index].classList.remove("hide");
 }
+
+
+
+
 window.onload=showItems();
 
-function moveSlide(direction) {
-    currentSlide += direction;
-
-    
-    if (currentSlide >= totalSlides) {
-        currentSlide = 0;
-    } else if (currentSlide < 0) {
-        currentSlide = totalSlides - 1;
-    }
-
-    const offset = -currentSlide * 100; // A slide eltolása
-    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
-}
